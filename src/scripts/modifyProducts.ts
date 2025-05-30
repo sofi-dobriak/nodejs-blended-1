8aededa0259fe74315ce2d97b243688d43ac9494
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-05-30 17:20:56 +0300

    fifth task is done

---
+++ src/scripts/modifyProducts.ts
@@ -0,0 +1,26 @@
+import fs from 'node:fs/promises';
+import { PATH_DB } from '../constants/products';
+import { Product } from '../types/product';
+
+const modifyProducts = async (): Promise<void> => {
+  try {
+    const data: string = await fs.readFile(PATH_DB, 'utf-8');
+
+    const products: Product[] = JSON.parse(data);
+
+    const modifyProducts: Product[] = products.map((product) => {
+      const { description, ...rest } = product;
+      return rest;
+    });
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify(modifyProducts, null, 2),
+      'utf-8',
+    );
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : String(error));
+  }
+};
+
+modifyProducts();
